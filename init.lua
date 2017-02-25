b861e41c8f38ebcc2e1f08bbf9d1e0145fbff23c
FaceDeer <derksenmobile@gmail.com>
2017-02-25 15:31:29 -0700

    make an internal shield-creation function.

--- init.lua
+++ init.lua
@@ -6,106 +6,42 @@ minetest.register_alias("castle:shield_3", "castle_shields:shield_3")
 local MP = minetest.get_modpath(minetest.get_current_modname())
 local S, NS = dofile(MP.."/intllib.lua")
 
-minetest.register_node("castle_shields:shield",{
+local register_shield = function(name, tile_side, tile_front, dye_1, dye_2)
+minetest.register_node("castle_shields:"..name, {
 	description = S("Mounted Shield"),
-	tiles = {"castle_shield_side.png", "castle_shield_side.png", "castle_shield_side.png", "castle_shield_side.png", "castle_shield_back.png", "castle_shield_front.png"},
+	tiles = {tile_side, tile_side, tile_side, tile_side, "castle_shield_back.png", tile_front},
 	drawtype="nodebox",
 	paramtype2 = "facedir",
 	paramtype = "light",
 	groups={cracky=3},
-	sounds = default.node_sound_defaults(),
+	sounds = default.node_sound_metal_defaults(),
 	node_box = {
 		type = "fixed",
 		fixed = {
-			{-0.500000,-0.125000,0.375000,0.500000,0.500000,0.500000}, --NodeBox 1
-			{-0.437500,-0.312500,0.375000,0.425000,0.500000,0.500000}, --NodeBox 2
-			{-0.312500,-0.437500,0.375000,0.312500,0.500000,0.500000}, --NodeBox 3
-			{-0.187500,-0.500000,0.375000,0.187500,0.500000,0.500000}, --NodeBox 4
+			{-0.500000,-0.125000,0.375000,0.500000,0.500000,0.500000},
+			{-0.437500,-0.312500,0.375000,0.425000,0.500000,0.500000},
+			{-0.312500,-0.437500,0.375000,0.312500,0.500000,0.500000},
+			{-0.187500,-0.500000,0.375000,0.187500,0.500000,0.500000},
 		},
 	},
 	selection_box = {
 		type = "fixed",
 		fixed = {
-			{-0.500000,-0.500000,0.375000,0.500000,0.500000,0.500000}, --NodeBox 1
-		},
-	},
-})
-
-
-minetest.register_craft({
-	output = "castle_shields:shield",
-	recipe = {
-		{"default:steel_ingot","default:steel_ingot","default:steel_ingot"},
-		{"default:steel_ingot", "default:steel_ingot","default:steel_ingot"},
-		{"dye:red", "default:steel_ingot","dye:blue"},
-	}
-})
-
-minetest.register_node("castle_shields:shield_2",{
-	description = S("Mounted Shield"),
-	tiles = {"castle_shield_side_2.png", "castle_shield_side_2.png", "castle_shield_side_2.png", "castle_shield_side_2.png", "castle_shield_back.png", "castle_shield_front_2.png"},
-	drawtype="nodebox",
-	paramtype2 = "facedir",
-	paramtype = "light",
-	groups={cracky=3},
-	sounds = default.node_sound_defaults(),
-	node_box = {
-		type = "fixed",
-		fixed = {
-			{-0.500000,-0.125000,0.375000,0.500000,0.500000,0.500000}, --NodeBox 1
-			{-0.437500,-0.312500,0.375000,0.425000,0.500000,0.500000}, --NodeBox 2
-			{-0.312500,-0.437500,0.375000,0.312500,0.500000,0.500000}, --NodeBox 3
-			{-0.187500,-0.500000,0.375000,0.187500,0.500000,0.500000}, --NodeBox 4
-		},
-	},
-	selection_box = {
-		type = "fixed",
-		fixed = {
-			{-0.500000,-0.500000,0.375000,0.500000,0.500000,0.500000}, --NodeBox 1
+			{-0.500000,-0.500000,0.375000,0.500000,0.500000,0.500000},
 		},
 	},
 })
 
 minetest.register_craft({
-	output = "castle_shields:shield_2",
+	output = "castle_shields:"..name,
 	recipe = {
-		{"default:steel_ingot","default:steel_ingot","default:steel_ingot"},
-		{"default:steel_ingot", "default:steel_ingot","default:steel_ingot"},
-		{"dye:cyan", "default:steel_ingot","dye:yellow"},
+		{"default:steel_ingot", "default:steel_ingot", "default:steel_ingot"},
+		{"default:steel_ingot", "default:steel_ingot", "default:steel_ingot"},
+		{dye_1, "default:steel_ingot", dye_2},
 	}
 })
+end
 
-minetest.register_node("castle_shields:shield_3",{
-	description = S("Mounted Shield"),
-	tiles = {"castle_shield_side_3.png", "castle_shield_side_3.png", "castle_shield_side_3.png", "castle_shield_side_3.png", "castle_shield_back.png", "castle_shield_front_3.png"},
-	drawtype="nodebox",
-	paramtype2 = "facedir",
-	paramtype = "light",
-	groups={cracky=3},
-	sounds = default.node_sound_defaults(),
-	node_box = {
-		type = "fixed",
-		fixed = {
-			{-0.500000,-0.125000,0.375000,0.500000,0.500000,0.500000}, --NodeBox 1
-			{-0.437500,-0.312500,0.375000,0.425000,0.500000,0.500000}, --NodeBox 2
-			{-0.312500,-0.437500,0.375000,0.312500,0.500000,0.500000}, --NodeBox 3
-			{-0.187500,-0.500000,0.375000,0.187500,0.500000,0.500000}, --NodeBox 4
-		},
-	},
-	selection_box = {
-		type = "fixed",
-		fixed = {
-			{-0.500000,-0.500000,0.375000,0.500000,0.500000,0.500000}, --NodeBox 1
-		},
-	},
-})
-
-minetest.register_craft({
-	output = "castle_shields:shield_3",
-	recipe = {
-		{"default:steel_ingot","default:steel_ingot","default:steel_ingot"},
-		{"default:steel_ingot", "default:steel_ingot","default:steel_ingot"},
-		{"dye:grey", "default:steel_ingot","dye:green"},
-	}
-})
-
+register_shield("shield", "castle_shield_side.png", "castle_shield_front.png", "dye:red", "dye:blue")
+register_shield("shield_2", "castle_shield_side_2.png", "castle_shield_front_2.png", "dye:cyan", "dye:yellow")
+register_shield("shield_3", "castle_shield_side_3.png", "castle_shield_front_3.png", "dye:grey", "dye:green")
