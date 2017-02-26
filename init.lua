e065d1e60fd48ee0721342d9414767de063f89d2
FaceDeer <derksenmobile@gmail.com>
2017-02-25 18:43:20 -0700

    moved aliases over to where shields are defined

--- init.lua
+++ init.lua
@@ -1,7 +1,3 @@
-minetest.register_alias("castle:shield", "castle_shields:shield_1")
-minetest.register_alias("castle:shield_2", "castle_shields:shield_2")
-minetest.register_alias("castle:shield_3", "castle_shields:shield_3")
-
 castle_shields = {}
 
 local MP = minetest.get_modpath(minetest.get_current_modname())
